6690fe078532a7db94d515bad5b7537c1255b44e
unknown <amansman77@gmail.com>
2019-10-10 15:13:19 +0900

    Pubsub consumer 예제에 string형 메세지 수신하는 예제 추가

--- src/main/java/com/ho/practice/kafka/pubsub/Consumer.java
+++ src/main/java/com/ho/practice/kafka/pubsub/Consumer.java
@@ -9,8 +9,13 @@ import com.ho.practice.kafka.message.Greeting;
 public class Consumer {
 	
 	@KafkaListener(topics = "${kafka.topic.pubsub}", containerFactory = "kafkaListenerContainerFactory")
-    public void listen(Greeting message) {
-    	System.out.println("Pubsub receive : " + message);
+    public void listen(String message) {
+    	System.out.println("[String] Pubsub receive : " + message);
+    }
+	
+	@KafkaListener(topics = "${kafka.topic.reqrep.request}", containerFactory = "jsonKafkaListenerContainerFactory")
+	public void listen2(Greeting message) {
+    	System.out.println("[Json] Pubsub receive : " + message);
     }
 	
 }
--- src/main/java/com/ho/practice/kafka/pubsub/KafkaConsumerConfig.java
+++ src/main/java/com/ho/practice/kafka/pubsub/KafkaConsumerConfig.java
@@ -11,12 +11,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
-
-import com.ho.practice.kafka.message.Greeting;
 
 /**
- * One way messaging에 대한 설정
+ * One way messaging에 대한 설정</br>
+ * 객체를 String형태로 받음
  * @author hhsung
  *
  */
@@ -27,23 +25,26 @@ public class KafkaConsumerConfig {
     private String bootstrapAddress;
 	
     @Bean
-    public ConsumerFactory<String, Greeting> consumerFactory() {
-    	 Map<String, Object> props = new HashMap<>();
-         props.put(
-           ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, 
-           bootstrapAddress);
-         props.put(
-           ConsumerConfig.GROUP_ID_CONFIG, 
-           "foo");
-        return new DefaultKafkaConsumerFactory<>(
-          props,
-          new StringDeserializer(), 
-          new JsonDeserializer<>(Greeting.class));
+    public ConsumerFactory<String, String> consumerFactory() {
+    	Map<String, Object> props = new HashMap<>();
+    	props.put(
+    			ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, 
+    			bootstrapAddress);
+    	props.put(
+    			ConsumerConfig.GROUP_ID_CONFIG, 
+    			"pubsub-consumer");
+    	props.put(
+    			ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, 
+    			StringDeserializer.class);
+    	props.put(
+    			ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, 
+    			StringDeserializer.class);
+    	return new DefaultKafkaConsumerFactory<>(props);
     }
     
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, Greeting> kafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, Greeting> factory = new ConcurrentKafkaListenerContainerFactory<>();
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
         return factory;
     }
